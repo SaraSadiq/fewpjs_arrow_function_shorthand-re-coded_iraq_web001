8ea385c6130c270cec54e728c48048c51d5fd261
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-07 19:05:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let divide = (parameter1, parameter2) => {
   console.log(`Adding ${parameter1}`);
   console.log(`Adding ${parameter2}`);
-  return parameter1 + parameter2;
+  return parameter1 / parameter2;
 }
-divide(1,2)
+divide(2000,100)
